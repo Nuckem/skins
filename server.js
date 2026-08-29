1125b7eb2e07f8633dd5a7441bdadea65b6210df
marius497 <149533471+marius497@users.noreply.github.com>
2026-08-29 19:07:03 +0300

    Enable trust proxy in server configuration

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
+
 const botClient = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
 });
